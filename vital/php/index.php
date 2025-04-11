736d8ba09798b06e3cc61b3dd551ac24716b71b7
baronigabriell <gabrielbaroni8@gmail.com>
2025-04-11 16:55:37 -0300

    Add files via upload

---
+++ vital/php/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Página inicial</title>
+</head>
+<body>
+<main id="main-index">
+    <div id="home-screen">
+        <button id="play-button">Jogar</button>
+    </div>
+
+    <div id="game-info" class="hidden">
+        <h1>Bem-vindo ao jogo!</h1>
+        <p>Aqui vão as informações do jogo...</p>
+    </div>
+</main>
+
+</body>
+<script>
+    const playButton = document.getElementById('play-button');
+    const homeScreen = document.getElementById('home-screen');
+    const gameInfo = document.getElementById('game-info');
+
+    playButton.addEventListener('click', () => {
+        playButton.style.animation = ''; 
+
+        playButton.style.animation = 'buttonDisappear 0.9s forwards';
+
+        setTimeout(() => {
+            homeScreen.style.display = 'none'; 
+            gameInfo.classList.remove('hidden');
+            gameInfo.classList.add('visible');
+        }, 900); 
+    });
+</script>
+
+</html>
